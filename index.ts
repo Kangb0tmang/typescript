25c5891a2f1c26bf9bf9bd126a57661dce1c8034
Kangb0tmang <Jon.Kang.JD@gmail.com>
2020-09-19 15:58:49 +1000

    Typescript basic types

--- index.ts
+++ index.ts
@@ -1,1 +1,20 @@
-console.log('hello world');
+const isOpen: boolean = false;
+const myName: string = 'Jon';
+const myAge: number = 28;
+const numList: number[] = [0, 1, 2, 3];
+
+// Tuple
+const person: [string, number, boolean] = ['Jon', 28, false];
+
+// Enum
+enum Job {
+  Dev,
+  UI,
+  UX,
+  Producer,
+}
+const job: Job = Job.Dev;
+
+// Any
+const phone: any = 'Google Pixel 2 XL';
+const android: any = 11;
